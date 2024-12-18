c225f3c198dbcf18b5e1dfd26f6d0700362441f4
Tomas Kirnig <tomas.kirnig.st@vsb.cz>
2024-12-18 14:48:03 +0100

    Added Component diagram

--- Presentation/obj/Debug/net8.0-windows/AnalyzeWindow.g.i.cs
+++ Presentation/obj/Debug/net8.0-windows/AnalyzeWindow.g.i.cs
@@ -120,13 +120,13 @@ namespace Presentation {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Presentation;component/analyzewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Presentation;V1.0.0.0;component/analyzewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AnalyzeWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -136,7 +136,7 @@ namespace Presentation {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
